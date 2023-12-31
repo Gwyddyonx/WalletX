16077f4bc01d1ae68fb20fded0b09e112c179d27
Gwyddyonx <50789646+Gwyddyonx@users.noreply.github.com>
2023-12-31 15:38:30 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Dashboard } from './componets/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Dashboard></Dashboard>
+    </div>
+  );
+}
+
+export default App;
